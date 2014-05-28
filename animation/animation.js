d781edac298ce257b5a3decd8a1d0307aefe5ead
Renaud Vincent <renaud.f.v@gmail.com>
2014-05-28 17:01:50 -0400

    Added Famo.us base kit tried static examples. Now doing integrat ion of app in Meteor with Famono package.

--- animation/animation.js
+++ animation/animation.js
@@ -1,15 +1,61 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to animation.";
-  };
+  require("famous-polyfills"); // Add polyfills
+  require("famous/core/famous"); // Add the default css file
+  var Engine = require("famous/core/Engine");
+  var Surface = require("famous/core/Surface");
+  var Modifier = require("famous/core/Modifier");
+  var GridLayout = require("famous/views/GridLayout");
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
+  var context = Engine.createContext();
+
+  var div = document.createElement('div');
+  var templates = [UI.insert(UI.render(Template.c1), div),
+  UI.insert(UI.render(Template.c2), div),
+  UI.insert(UI.render(Template.c3), div),
+  UI.insert(UI.render(Template.c4), div)];
+
+  console.log(templates);
+  var modifier = new Modifier({ 
+    origin: [0.5, 0.5] 
   });
+  // var div = document.createElement('div');
+  // UI.insert(UI.render(Template.content), div);
+
+  // var surface = new Surface({ 
+  //     content: div, 
+  //     size: [250, 250], 
+  //     properties: { 
+  //         border: "1px solid"
+  //     } 
+  // });
+
+  // context.add(modifier).add(surface);
+
+
+  var grid = new GridLayout({
+    dimensions: [2, 2]
+  });
+
+  var surfaces = [];
+  grid.sequenceFrom(surfaces);
+
+  for(var i = 0; i < templates.length; i++) {
+    surfaces.push(new Surface({
+      content: templates[i],
+      size: [250, 250],
+      properties: {
+        border: "1px dotted",
+        borderRadius: "10px",
+        backgroundColor: "hsl(" + (1 * 360 / 8) + ", 100%, 50%)",
+        color: "black",
+        lineHeight: window.innerHeight / 2 + 'px',
+        textAlign: 'center'
+      }
+    }));
+  }
+
+   context.add(modifier).add(grid);
+
 }
 
 if (Meteor.isServer) {
