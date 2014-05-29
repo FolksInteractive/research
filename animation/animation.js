3b35c26be2d0dde661b75ca349623529d9ad8891
Renaud Vincent <renaud.f.v@gmail.com>
2014-05-29 17:21:25 -0400

    App runs for one click sequence.

--- animation/animation.js
+++ animation/animation.js
@@ -31,7 +31,6 @@ if (Meteor.isClient) {
   addContent();
 
   function createGrid() {
-
     var grid = new GridLayout({
       dimensions: [2, 2]
     });
@@ -39,12 +38,15 @@ if (Meteor.isClient) {
     grid.sequenceFrom(surfaces);
 
     for(var i = 0; i < templates.length; i++) {
+      if(surfaces.length >= 4)
+        surfaces = []
       surfaces.push(new Surface({
         content: templates[i],
         size: [440, 380],
         properties: {
           border: "1px solid grey",
           borderRadius: "20px",
+          textAlign: "center",
           color: "black"
         }
       }));
@@ -54,44 +56,62 @@ if (Meteor.isClient) {
       
       surfaces[j].on('click', function() {
         var focus = new StateModifier({
-              origin: [0.5,0.5]
+          origin: [0.5,0.5],
+          opacity: 0
         });
         currentTile = this;
+        console.log(currentTile);
 
         Session.set('tile', true);
         contentModifier.setTransform(
           Transform.scale(0, 0, 0),
-          { duration : 1000, curve: Easing.inExpo },
+          { duration : 10, curve: Easing.inExpo },
           function() {
             console.log("HELLO");
             currentTile.setSize([undefined,undefined]);
+            // currentTile.setProperties({
+            //   fontSize: "30px"
+            // });
+            currentTile.addClass('selected');
             focus.setTransform(
               Transform.scale(1, 1, 0),
-              { duration : 500, curve: Easing.inExpo }
+              { duration : 100, curve: Easing.inExpo },
+              function() {
+                focus.setOpacity(1,
+                  { duration : 500, curve: Easing.inExpo }
+                );
+                
+              }
             );
-            
 
             layout.content.add(focus).add(currentTile);
             
           }
         );
-        glyph.on('click', function() {
+        
+
+      }); 
+
+    }
+    return grid;
+  }
+  glyph.on('click', function() {
+    console.log(surfaces);
+
           if(Session.get('tile')) {
             contentModifier.setTransform(
               Transform.scale(1, 1, 0),
               { duration : 1000, curve: Easing.inExpo }
               );
             console.log(context);
-            layout.content.add(contentModifier).add(surfaces);
+
+            currentTile.removeClass('selected');
+            currentTile.setSize([440,380]);
+            layout.content.add(contentModifier).add(createGrid());
             Session.set('tile', undefined);
           }
+          console.log("GLYPOH");
         });
-
-      });  
-    }
-
-    return grid;
-  }
 
   function createLayout() {
     layout = new HeaderFooterLayout({
