7262db372910251905962503eb859348c3f0b2c1
Renaud Vincent <renaud.f.v@gmail.com>
2014-06-02 17:23:47 -0400

    Added side-menu

--- animation/animation.js
+++ animation/animation.js
@@ -18,8 +18,15 @@ if (Meteor.isClient) {
   var context = Engine.createContext();
   var layout;
   var selected = true;
+  var menu = false;
 
   var enventHandler = new EventHandler();
+
+  var itemsGlyph = ['<span class="glyphicon glyphicon-align-left"></span>',
+  '<span class="glyphicon glyphicon-align-center"></span>',
+  '<span class="glyphicon glyphicon-align-right"></span>',
+  '<span class="glyphicon glyphicon-align-justify"></span>',
+  '<span class="glyphicon glyphicon-list"></span>'];
 
   /**********
   * MODIFIERS
@@ -34,9 +41,16 @@ if (Meteor.isClient) {
     align: [0,0]
   });
 
+  var menuModifier = new StateModifier({
+    origin: [0,0],
+    transform: Transform.translate(0, 0, 1)
+  });
+
   var glyphModifier = new StateModifier();
 
   var view2Modifiers = [];
+
+  var itemsModifiers = [];
   
   /***************
   * SURFACES/VIEWS
@@ -49,6 +63,10 @@ if (Meteor.isClient) {
   view1.add(createGrid());
 
   var view2 = new View();
+
+  var menuView = new View();
+  createMenuItems();
+  
 
   var glyph = new Surface({
     size: [undefined,55],
@@ -74,11 +92,11 @@ if (Meteor.isClient) {
   /******
   * CALLS
   *******/  
-
   createLayout();
   addHeader();
   layout.content.add(mainModifier).add(view1);
   fillView2();
+  layout.content.add(menuModifier).add(menuView);
   handleEvents();
 
   /**********
@@ -92,12 +110,15 @@ if (Meteor.isClient) {
     context.add(layout);
   }
 
-  
   function addHeader() {
     var head = loadTemplate(Template.header);
 
     glyph.on('click', function() {
       enventHandler.emit('glyph');
+    });
+
+    menu.on('click',function() {
+      enventHandler.emit('menu');
     });
 
     layout.header.add(menu);
@@ -108,7 +129,6 @@ if (Meteor.isClient) {
         zIndex: '0'
       }
     }));
-
   }
 
   function fillView2() {
@@ -121,87 +141,79 @@ if (Meteor.isClient) {
     view2.add(view2Modifiers[i]).add(new Surface({classes: ['selected'], content: templates[i]}));
   }
   layout.content.add(view2);
+  }
 
-  console.log(view2);
-}
+  function createGrid() {
+    var grid = new GridLayout({
+      dimensions: [2, 2]
+    });
 
-function createGrid() {
-  var grid = new GridLayout({
-    dimensions: [2, 2]
-  });
+    grid.sequenceFrom(tiles);
 
-  grid.sequenceFrom(tiles);
+    return grid;
+  }
 
-  return grid;
-}
+  function createMenuItems() {
+    var items = [];
 
-function createTiles() {
+    var height = 0;
+    for(var i = 0; i < 5; i++) {
+      items.push(new Surface({
+        size: [45,30],
+        content: itemsGlyph[i],
+        properties: {
+          fontSize: '30px',
+          paddingLeft: '15px'
+        }
+      }));
+      itemsModifiers.push(new StateModifier({
+        transform: Transform.translate(-50,height,0)
+      }));
+      menuView.add(itemsModifiers[i]).add(items[i]);
+      height = height + 75;
+    }
+
+  }
+
+  function createTiles() {
   var surfaces = [];
   var templates = [loadTemplate(Template.c1), loadTemplate(Template.c2),
   loadTemplate(Template.c3), loadTemplate(Template.c4)];
 
   for(var i = 0; i < templates.length; i++) {
-    // if(surfaces.length >= 4)
-    surfaces.push(new Surface({
-      origin: [0.5,0.5],
-      content: templates[i],
-      size: [440, undefined],
-      classes: ['tile'],
-      properties: {
-        border: "1px solid grey",
-        // borderRadius: "20px",
-        textAlign: "center",
-        color: "black",
-        backgroundColor: 'white',
-        overflow: 'hidden'
-      }
-    }));
-    surfaces[i].on('click', function() {
-      enventHandler.emit('tile', this);
-    });
+      // if(surfaces.length >= 4)
+      surfaces.push(new Surface({
+        origin: [0.5,0.5],
+        content: templates[i],
+        size: [440, undefined],
+        classes: ['tile'],
+        properties: {
+          border: "1px solid grey",
+          // borderRadius: "20px",
+          textAlign: "center",
+          color: "black",
+          backgroundColor: 'white',
+          overflow: 'hidden'
+        }
+      }));
+      surfaces[i].on('click', function() {
+        enventHandler.emit('tile', this);
+      });
+    }
+    return surfaces;
   }
-  return surfaces;
-}
 
-function handleEvents() {
-  enventHandler.on('tile', function(tile) {
+  function handleEvents() {
+    enventHandler.on('tile', function(tile) {
 
-    currentTile = tile.id;
+      currentTile = tile.id;
 
-    view2Modifiers[currentTile].setOpacity(
-      1, 
-      {duration: 1000}
-      );
-    headModifier.setTransform(
-      Transform.translate(0,-1000,0),
-      { duration : 1000, curve: Easing.inOutBack }
-      );
-    glyphModifier.setOpacity(
-      0,
-      {duration : 500, curve: Easing.inSine },
-      function() {
-        glyph.setProperties({
-          textAlign: 'right',
-          paddingRight: '15px'
-        });
-        glyphModifier.setOpacity(1, {duration : 500});
-      }
-      );
-
-    slideRight(mainModifier);
-
-    selected = true;
-  });
-
-  enventHandler.on('glyph', function() {
-
-    if(selected) {
       view2Modifiers[currentTile].setOpacity(
-        0, 
+        1, 
         {duration: 1000}
         );
       headModifier.setTransform(
-        Transform.translate(0,0,0),
+        Transform.translate(0,-1000,0),
         { duration : 1000, curve: Easing.inOutBack }
         );
       glyphModifier.setOpacity(
@@ -209,39 +221,95 @@ function handleEvents() {
         {duration : 500, curve: Easing.inSine },
         function() {
           glyph.setProperties({
-            textAlign: 'center'
+            textAlign: 'right',
+            paddingRight: '15px'
           });
           glyphModifier.setOpacity(1, {duration : 500});
         }
         );
 
-      slideLeft(mainModifier);
+      slideRight(mainModifier);
 
-      selected = false;
-    }
+      selected = true;
+    });
 
-  });
-}
+    enventHandler.on('glyph', function() {
 
-function loadTemplate(template) {
-  var div = document.createElement('div');
-  UI.insert(UI.render(template), div);
-  return div;
-}
+      if(selected) {
+        view2Modifiers[currentTile].setOpacity(
+          0, 
+          {duration: 1000}
+          );
+        headModifier.setTransform(
+          Transform.translate(0,0,0),
+          { duration : 1000, curve: Easing.inOutBack }
+          );
+        glyphModifier.setOpacity(
+          0,
+          {duration : 500, curve: Easing.inSine },
+          function() {
+            glyph.setProperties({
+              textAlign: 'center'
+            });
+            glyphModifier.setOpacity(1, {duration : 500});
+          }
+          );
 
-function slideRight(modifier) {
-  modifier.setTransform(
-    Transform.translate(10000,0,0.1),
-    { duration: 1000, curve: Easing.inSine }
-    );
-}
+        slideLeft(mainModifier);
 
-function slideLeft(modifier) {
-  modifier.setTransform(
-    Transform.translate(0,0,0.1),
-    { duration: 1000, curve: Easing.inSine }
-    );
-}
+        selected = false;
+      }
+
+    });
+
+    enventHandler.on('menu', function() {
+      if(menu) {
+        var time = 100;
+        var height = 0;
+        for(var i = 0; i < itemsModifiers.length; i++) {
+          itemsModifiers[i].setTransform(Transform.translate(0,height,0), {duration: time, curve: Easing.inOutSine});
+          if(view2Modifiers[i])
+            view2Modifiers[i].setTransform(Transform.translate(50,0,0), {duration: 200, curve: Easing.inOutSine});
+          time = time + 100;
+          height = height + 75;
+        }
+      }
+
+      else {
+        var time = 100;
+        var height = 0;
+        for(var i = 0; i < itemsModifiers.length; i++) {
+          itemsModifiers[i].setTransform(Transform.translate(-50,height,0), {duration: time, curve: Easing.inOutSine});
+          if(view2Modifiers[i])
+            view2Modifiers[i].setTransform(Transform.translate(0,0,0), {duration: 200, curve: Easing.inOutSine});
+          time = time + 100;
+          height = height + 75;
+        }
+      }
+
+      menu = !menu;
+    });
+  }
+
+  function loadTemplate(template) {
+    var div = document.createElement('div');
+    UI.insert(UI.render(template), div);
+    return div;
+  }
+
+  function slideRight(modifier) {
+    modifier.setTransform(
+      Transform.translate(10000,0,0.1),
+      { duration: 1000, curve: Easing.inSine }
+      );
+  }
+
+  function slideLeft(modifier) {
+    modifier.setTransform(
+      Transform.translate(0,0,0.1),
+      { duration: 1000, curve: Easing.inSine }
+      );
+  }
 
 }
 
